e302ffc613068281252c4519cac0421d4b910ec9
Noufal Ibrahim <noufal@nibrahim.net.in>
2012-12-02 13:52:25 +0530

    Fixed deploy and jekyll commands for Octopress

--- .hyde.el
+++ .hyde.el
@@ -1,1 +1,4 @@
-(setq hyde/git/remote "origin")
+(setq hyde/deploy-command "rsync -vr _site/* nkv@ssh.hcoop.net:/afs/hcoop.net/user/n/nk/nkv/public_html/nibrahim.net.in/"
+      hyde/jekyll-command "rvm ruby-1.9.2-p290 exec jekyll"
+)
+
